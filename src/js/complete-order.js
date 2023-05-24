5af4728015b5cb81abb934d690bde3383a166c46
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-05-25 00:30:57 +0300

    Update complete-order.js

--- src/js/complete-order.js
+++ src/js/complete-order.js
@@ -2,13 +2,14 @@ renderOrderList();
 
 function renderOrderList() {
   const orderList = document.querySelector(".basket-list");
-  let path = './src/img/';
+  let path = "./src/img/";
   let totalSum = 0;
 
-  orderList.innerHTML = '';
+  orderList.innerHTML = "";
 
   cart.forEach(item => {
-    const { id, name, price, image, numberOfUnits, pizzaSize, ingredients } = item;
+    const { id, name, price, image, numberOfUnits, pizzaSize, ingredients } =
+      item;
 
     orderList.innerHTML += `
       <div class="basket-item">
@@ -41,92 +42,96 @@ function renderOrderList() {
 }
 
 // Choose delivery type option (delivery or carry out)
-const deliveryOption = document.getElementById('deliveryOption');
-const carryoutOption = document.getElementById('carryoutOption');
-const deliveryOptionBlock = document.querySelector('.deliveryOption__info');
-const carryoutOptionBlock = document.querySelector('.carryoutOption__info');
+const deliveryOption = document.getElementById("deliveryOption");
+const carryoutOption = document.getElementById("carryoutOption");
+const deliveryOptionBlock = document.querySelector(".deliveryOption__info");
+const carryoutOptionBlock = document.querySelector(".carryoutOption__info");
 const label1 = document.querySelector('label[for="deliveryOption"]');
 const label2 = document.querySelector('label[for="carryoutOption"]');
 
 chooseTypeOfDelivery();
 
-deliveryOption.addEventListener('change', chooseTypeOfDelivery);
-carryoutOption.addEventListener('change', chooseTypeOfDelivery);
+deliveryOption.addEventListener("change", chooseTypeOfDelivery);
+carryoutOption.addEventListener("change", chooseTypeOfDelivery);
 
 function chooseTypeOfDelivery() {
   if (deliveryOption.checked && !carryoutOption.checked) {
-    deliveryOptionBlock.style.display = 'block';
-    carryoutOptionBlock.style.display = 'none';
-    label1.classList.add('active');
-    label2.classList.remove('active');
+    deliveryOptionBlock.style.display = "block";
+    carryoutOptionBlock.style.display = "none";
+    label1.classList.add("active");
+    label2.classList.remove("active");
   }
   if (carryoutOption.checked && !deliveryOption.checked) {
-    deliveryOptionBlock.style.display = 'none';
-    carryoutOptionBlock.style.display = 'block';
-    label2.classList.add('active');
-    label1.classList.remove('active');
+    deliveryOptionBlock.style.display = "none";
+    carryoutOptionBlock.style.display = "block";
+    label2.classList.add("active");
+    label1.classList.remove("active");
   }
 }
 
 // Display total sum at checkout
 function showCheckoutTotal(totalSum) {
-  const element = document.querySelector('.js-checkout-total');
-  element.innerHTML = totalSum + ' UAH';
+  const element = document.querySelector(".js-checkout-total");
+  element.innerHTML = totalSum + " UAH";
 }
 
 // Click to return to main page
-const returnToMenuBtn = document.getElementById('js-return-to-menu-btn');
+const returnToMenuBtn = document.getElementById("js-return-to-menu-btn");
 
-returnToMenuBtn.addEventListener('click', () => {
-  location.href = '/index.html';
+returnToMenuBtn.addEventListener("click", () => {
+  location.href = "/index.html";
 });
 
-// 
-const checkoutBtn = document.getElementById('js-checkout-btn');
-const submitOrderBtn = document.getElementById('js-submit-order');
-const paymentBlock = document.querySelector('.payment');
-const returnToCheckoutBtn = document.getElementById('js-return-to-checkout-btn');
+//
+const checkoutBtn = document.getElementById("js-checkout-btn");
+const submitOrderBtn = document.getElementById("js-submit-order");
+const paymentBlock = document.querySelector(".payment");
+const returnToCheckoutBtn = document.getElementById(
+  "js-return-to-checkout-btn"
+);
 
-returnToCheckoutBtn.addEventListener('click', () => {
-  paymentBlock.classList.remove('active');
-})
+returnToCheckoutBtn.addEventListener("click", () => {
+  paymentBlock.classList.remove("active");
+});
 
 // Click Checkout button to proceed to checkout
-checkoutBtn.addEventListener('click', goToCheckout);
+checkoutBtn.addEventListener("click", goToCheckout);
 
 function goToCheckout() {
-  const requiredFields = document.querySelectorAll('.basket-form input[required]');
+  const requiredFields = document.querySelectorAll(
+    ".basket-form input[required]"
+  );
 
   requiredFields.forEach(input => {
-    if (input.value === '') {
-      input.nextElementSibling.innerHTML = 'This field is required';
-      return ok = false;
+    if (input.value === "") {
+      input.nextElementSibling.innerHTML = "This field is required";
+      return (ok = false);
     } else {
-      input.nextElementSibling.innerHTML = '';
-      return ok = true;
+      input.nextElementSibling.innerHTML = "";
+      return (ok = true);
     }
   });
 
   if (ok) {
-    paymentBlock.classList.add('active');
+    paymentBlock.classList.add("active");
   }
 }
 
 // Click Submit button to submit your order
-submitOrderBtn.addEventListener('click', (event) => {
-  const requiredFields = document.querySelectorAll('.payment input[required]');
-  const email = document.getElementById('to');
+submitOrderBtn.addEventListener("click", event => {
+  const requiredFields = document.querySelectorAll(".payment input[required]");
+  const email = document.getElementById("to");
 
   requiredFields.forEach(el => {
-    if (el.value === '') {
-      el.nextElementSibling.innerHTML = 'This field is required';
-      return ok = false;
+    if (el.value === "") {
+      el.nextElementSibling.innerHTML = "This field is required";
+      return (ok = false);
     } else {
-      el.nextElementSibling.innerHTML = '';
-      return ok = true;
+      el.nextElementSibling.innerHTML = "";
+      return (ok = true);
     }
   });
-  
+
   if (ok) {
     sendEmail(event);
   }
@@ -134,8 +139,8 @@ submitOrderBtn.addEventListener('click', (event) => {
 
 // Validation block
 // When user is typing tel number display if it's invalid
-const tel = document.getElementById('tel');
-tel.addEventListener('input', () => {
+const tel = document.getElementById("tel");
+tel.addEventListener("input", () => {
   validateTelNumber(tel);
 });
 
@@ -143,15 +148,15 @@ tel.addEventListener('input', () => {
 function validateTelNumber(tel) {
   const telRegexp = /^(\+38|38)?0(39|50|63|66|67|68|73|89|9[1-9])[0-9]{7}$/;
 
-  if (tel.value === '') {
+  if (tel.value === "") {
     return;
   }
   if (telRegexp.test(tel.value)) {
-    tel.nextElementSibling.innerHTML = '';
-    return true
+    tel.nextElementSibling.innerHTML = "";
+    return true;
   } else {
-    tel.nextElementSibling.innerHTML = 'Please enter valid mobile number';
-    return false
+    tel.nextElementSibling.innerHTML = "Please enter valid mobile number";
+    return false;
   }
 }
 
@@ -160,9 +165,7 @@ function validateEmail(email) {
   let emailRegexp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   if (emailRegexp.test(email.value)) {
     return true;
-  }
-  else {
+  } else {
     return false;
   }
 }
-
