42759d62282484e1c584603344853beaac3af126
Anton Chan <106675165+chanchik96@users.noreply.github.com>
2023-05-24 19:23:46 +0300

    Create script.js

---
+++ src/js/script.js
@@ -0,0 +1,88 @@
+// All nav buttons defining
+const navBtns = document.querySelectorAll('.header__nav-item');
+
+
+// Get header height
+const header = document.querySelector('.header');
+const headerHeight = header.offsetHeight;
+
+
+// Scroll to section
+navBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    let value = btn.innerHTML;
+    let scrollDiv = document.getElementById(`${value}`).offsetTop;
+    window.scrollTo({ top: (scrollDiv - headerHeight), behavior: 'smooth' });
+  })
+})
+
+// Hamburger menu
+const menuBtn = document.getElementById('hamburger-menu');
+const mobileMenu = document.querySelector('.mobile-nav');
+
+menuBtn.addEventListener('click', () => {
+  menuBtn.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+});
+
+// Open and close shopping cart
+const cartBtn = document.getElementById("open-cart-btn");
+const cartBox = document.querySelector('.cart');
+const closeCartBtn = document.getElementById('close-cart-btn');
+
+cartBtn.addEventListener('click', () => {
+  cartBox.classList.add('active');
+})
+
+closeCartBtn.addEventListener('click', () => {
+  cartBox.classList.remove('active')
+});
+
+// go to homepage
+function goToHomePage() {
+  window.location.href = '../index.html'
+}
+
+// function for sending emails via Mailgun
+async function sendEmail(event) {
+  event.preventDefault();
+
+  const apiKey = 'b6b7fa3b3eef039047ee65f5fe973c73-102c75d8-21f26b79';
+  const domain = 'antonchan.xyz';
+  const from = 'napizza@antonchan.xyz';
+
+  const to = document.getElementById('to').value;
+  const subject = 'Mail check';
+  const body = `Hello!\n 
+  Thank you for your order`;
+
+  const formData = new FormData();
+  formData.append('from', from);
+  formData.append('to', to);
+  formData.append('subject', subject);
+  // formData.append('v:name', name);
+  formData.append('text', body);
+
+  try {
+    const response = await fetch(`https://api.mailgun.net/v3/${domain}/messages`, {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + btoa('api:' + apiKey)
+      },
+      body: formData
+    });
+
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      // alert('Email sent successfully!');
+      orderConfirmedModal('Your order is confirmed');
+      clearAll();
+    } else {
+      console.error('Error sending email:', response.status, response.statusText);
+      // alert('Error sending email. Please check the console for more details.');
+    }
+  } catch (error) {
+    console.error('Error sending email:', error);
+    // alert('Error sending email. Please check the console for more details.');
+  }
+}
